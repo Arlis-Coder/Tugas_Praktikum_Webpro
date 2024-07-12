29554fd1bf8c952522bdc1b8ff9cde89cd3484da
Caveman-cell <nokalate@gmail.com>
2024-07-12 14:27:38 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('siswa', 'SiswaController::index');
+$routes->post('siswa/simpan', 'SiswaController::simpan');
+$routes->get('siswa/hapus/(:segment)', 'SiswaController::hapus/$1');
+$routes->get('siswa/edit/(:segment)', 'SiswaController::edit/$1');
